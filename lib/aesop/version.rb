c54d12aa560dc1751cb3f30c2ba4bbf853436492
J.W. Koelewijn <jwkoelewijn@gmail.com>
2014-11-11 16:02:59 +0100

    Increased version

--- lib/aesop/version.rb
+++ lib/aesop/version.rb
@@ -3,7 +3,7 @@ module Aesop
     MAJOR = 1
     MINOR = 1
     PATCH = 0
-    BUILD = 2
+    BUILD = 3
   end
 
   VERSION = [Version::MAJOR, Version::MINOR, Version::PATCH, Version::BUILD].compact.join('.')
